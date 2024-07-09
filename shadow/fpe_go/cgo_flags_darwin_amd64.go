6db37621fa90e552452ff2d294056e1a303c48e8
Jianzhang.kk <jianzhang.kk@bytedance.com>
2024-07-09 18:00:37 +0800

    add link cfg for darwin amd64

--- shadow/fpe_go/cgo_flags_darwin_amd64.go
+++ shadow/fpe_go/cgo_flags_darwin_amd64.go
@@ -2,5 +2,5 @@
 
 package fpe
 
-// #cgo LDFLAGS: -L${SRCDIR}/lib -lshadow_fpe_export_darwin_amd64 -Wl,-rpath,${SRCDIR}/lib
+// #cgo LDFLAGS: -L${SRCDIR}/lib -lshadow_fpe_export_darwin_amd64 -lm -lstdc++ -Wl,-rpath,${SRCDIR}/lib
 import "C"
